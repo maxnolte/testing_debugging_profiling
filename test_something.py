22bc92686d7971a193a921142148634f0f7e2f85
ASPP Student <student@localhost>
2018-09-03 18:08:56 +0200

    initial tests done

--- test_something.py
+++ test_something.py
@@ -1,3 +1,6 @@
+from math import isclose
+
+
 def test_arithmetic():
     assert 1 == 1
     assert 2 * 3 == 6
@@ -5,3 +8,9 @@ def test_arithmetic():
 def test_len_list():
     lst = ['a', 'b', 'c']
     assert len(lst) == 3
+
+def test_sum():
+    assert 1 + 2 == 3
+
+def test_sum_float():
+    assert isclose(1.1 + 2.2, 3.3)
